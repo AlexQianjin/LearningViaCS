93a9dcee06738399f2b265544f7671d99bb7a2ef
Alex Qin <qianjin.qin@qq.com>
2017-03-24 07:37:27 -0700

    add Proxy Pattern

---
+++ DesignPattern/Proxy/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Proxy
+{
+    class Program:SubjectAccessor
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Proxy Pattern\n");
+
+            ISubject subject = new Proxy();
+            Console.WriteLine(subject.Request());
+            Console.WriteLine(subject.Request());
+
+            ISubject protectionSubject = new ProtectionProxy();
+            Console.WriteLine(protectionSubject.Request());
+            Console.WriteLine((protectionSubject as ProtectionProxy).Authenticate("Secret"));
+            Console.WriteLine((protectionSubject as ProtectionProxy).Authenticate("Abracadabra"));
+            Console.WriteLine(protectionSubject.Request());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DesignPattern/Proxy/SubjectAccessor.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proxy
+{
+    public class SubjectAccessor
+    {
+        public interface ISubject
+        {
+            string Request();
+        }
+
+        private class Subject
+        {
+            public string Request()
+            {
+                return "Subject Request " + "Choose left door\n";
+            }
+        }
+
+        public class Proxy : ISubject
+        {
+            private Subject subject;
+
+            public string Request()
+            {
+                if (subject == null)
+                {
+                    Console.WriteLine("Subject inactive");
+                    subject = new Subject();
+                }
+                Console.WriteLine("Subject active");
+                return "Proxy: Call to " + subject.Request();
+            }
+        }
+
+        public class ProtectionProxy : ISubject
+        {
+            private Subject subject;
+            private string password = "Abracadabra";
+
+            public string Authenticate(string supplied)
+            {
+                if (supplied != password) 
+                {
+                    return "Protection Proxy: No access";
+                }
+                else
+                {
+                    subject = new Subject();
+                    return "Protection Proxy: Authenticated";
+                }
+            }
+
+            public string Request()
+            {
+                if (subject == null)
+                {
+                    return "Protection Proxy: Authenticate first";
+                }
+                else
+                {
+                    return "Protection Proxy: Call to " + subject.Request();
+                }
+            }
+        }
+    }
+}
